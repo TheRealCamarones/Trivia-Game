dab9a582966aa06f6dc63c3728289df45e917e56
Camarones <cameron_walther@yahoo.com>
2019-02-25 15:59:18 -0500

    new game button click event good to go, fixed glitch where the timer would compound, need to add my clear function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,8 +39,10 @@ var incorrect = 0;
 // set variables to manipulate the DOM
 var questionDisplay;
 var answerDisplay;
+var userGuess;
 var timeRemaining;
 var theClock;
+var answerValue;
 
 $("document").ready(function () {
     function newGame() {
@@ -50,27 +52,37 @@ $("document").ready(function () {
         questionCounter = 0;
         timer = 25;
         displayQuestions();
-        
+        theTimer();
     }
 
     function displayQuestions() {
         // display the timer
         timeRemaining = $("#time-remaining-display").text("Time Remaining: " + timer);
         // I had nested loops in here to display the questions and answers, but that wasn't working right
-
+        // pretty sure i'll just end up incrementing the questionCounter and then running this function again
         questionDisplay = $("#display-question").append(questionsArray[questionCounter]);
 
+        // have a loop that for every answer option only associated with current question counter creates a new div
+        // with class of answer-choice, attr of the correct answer sets the HTML to the answer option A-D and then appends
         for (var i = 0; i < answersArray.length; i++) {
             answerDisplay = $("<div>");
             answerDisplay.addClass("answer-choice");
+            answerDisplay.attr(answerValue, answersArray[questionCounter][i]);
             answerDisplay.html(answersArray[questionCounter][i]);
             $(".answer-display").append(answerDisplay);
         }
-        theTimer();
+
+        // add something to run here to grab the value of the choice the user clicks on 
+        $("answer-choice").on("click", function () {
+            userGuess = $(this).attr(answerValue);
+            console.log(answerValue);
+        })
+
     }
 
     function theTimer() {
         // set the clock to an interval of 25 seconds
+        clearInterval(theClock);
         theClock = setInterval(decrement, 1000);
         function decrement() {
             if (timer === 0) {
@@ -92,5 +104,13 @@ $("document").ready(function () {
 
     };
 
-    $("#new-game-button").on("click", newGame())
+    // this function will end up being called a lot as we need to wipe the screen clear of data
+    function clear() {
+
+    }
+
+    // event handler for the newGame button
+    $("#new-game-button").on("click", function () {
+        newGame();
+    })
 });
