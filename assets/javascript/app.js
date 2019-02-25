939977ee3c555e4ada6d7f33bd6f469628d12d71
Camarones <cameron_walther@yahoo.com>
2019-02-25 17:19:32 -0500

    I made an object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,32 @@
-// thought about creating an array of objects, but I think it would be simpler for my understanding to make three arrays
-var questionsArray = ["Who won the first Super Bowl?",
-    "Who is the NHL's All Time leading goal scorer?",
-    "Which team has won the most NBA Championships?",
-    "Who is the PGA Tour's All Time Leading Money Earner?",
-    "Who is the MLB pitcher with the most career wins?"];
-
-// This one will be an array of arrays
-var answersArray = [
-    ["The Minnesota Vikings", "The Green Bay Packers", "The Saskatoon Snowmen", "The Chicago Cubs"],
-    ["Jaromir Jagr", "Sidney Crosby", "Mario Lemieux", "Wayne Gretzky"],
-    ["Los Angeles Lakers", "Chicago Bulls", "Boston Celtics", "Harlem Globetrotters"],
-    ["Vijay Singh", "Tiger Woods", "Ernie Els", "Jordan Spieth"],
-    ["Cy Young", "Randy Johnson", "Babe Ruth", "Jim Abbott"]
-];
-
-var correctAnswerArray = ["The Green Bay Packers", "Wayne Gretzky", "Boston Celtics", "Tiger Woods", "Cy Young"];
-
-// create an array to store all of my images
-// pretty DRY, but I can't think of a better way to do what I'm trying to do
-var imgArray = new Array();
-imgArray[0] = "<img class='d-block mx-auto' src='../images/greenbaypackers.jpg'>";
-imgArray[1] = "<img class='d-block mx-auto' src='../images/gretzky.jpg'>";
-imgArray[2] = "<img class='d-block mx-auto' src='../images/bostonceltics.jpg'>";
-imgArray[3] = "<img class='d-block mx-auto' src='../images/tigerwoods.jpg'>";
-imgArray[4] = "<img class='d-block mx-auto' src='../images/cyyoung.jpg'>";
-
-// and a variable for an image that I may or may not actually ever use
-var schwabImage = "<img class='d-block mx-auto' src='../images/theschwab.jpg'>";
+// array of arrays sucks let's make an object
+var questions = [{
+    question: "Who won the first Super Bowl?",
+    answers: ["The Minnesota Vikings", "The Green Bay Packers", "The Saskatoon Snowmen", "The Chicago Cubs"],
+    correct: 1,
+    image: "<img class='d-block mx-auto' src='../images/greenbaypackers.jpg'>"
+}, {
+    question: "Who is the NHL's All time leading goal scorer?",
+    answers: ["Jaromir Jagr", "Sidney Crosby", "Mario Lemieux", "Wayne Gretzky"],
+    correct: 3,
+    image: "<img class='d-block mx-auto' src='../images/gretzky.jpg'>",
+}, {
+    question: "Which team has won the most NBA Championships?",
+    answers: ["Los Angeles Lakers", "Chicago Bulls", "Boston Celtics", "Harlem Globetrotters"],
+    correct: 2,
+    image: "<img class='d-block mx-auto' src='../images/bostonceltics.jpg'>",
+}, {
+    question: "Who is the PGA Tour All Time Leading Money Earner?",
+    answers: ["Vijay Singh", "Tiger Woods", "Ernie Els", "Jordan Spieth"],
+    correct: 1,
+    image: "<img class='d-block mx-auto' src='../images/tigerwoods.jpg'>",
+}, {
+    question: "Who is the MLB pither with the most career wins?",
+    answers: ["Cy Young", "Randy Johnson", "Babe Ruth", "Jim Abbott"],
+    correct: 0,
+    image: "<img class='d-block mx-auto' src='../images/cyyoung.jpg'>",
+}, {
+    schwabImage : "<img class='d-block mx-auto' src='../images/theschwab.jpg'>",
+}];
 
 // set a timer globally that we'll keep coming back to
 var timer = 25;
@@ -51,6 +51,7 @@ $("document").ready(function () {
         gameRunning = true;
         questionCounter = 0;
         timer = 25;
+        clear();
         displayQuestions();
         theTimer();
     }
@@ -60,25 +61,26 @@ $("document").ready(function () {
         timeRemaining = $("#time-remaining-display").text("Time Remaining: " + timer);
         // I had nested loops in here to display the questions and answers, but that wasn't working right
         // pretty sure i'll just end up incrementing the questionCounter and then running this function again
-        questionDisplay = $("#display-question").append(questionsArray[questionCounter]);
+        questionDisplay = $("#display-question").append(questions[questionCounter].question);
 
         // have a loop that for every answer option only associated with current question counter creates a new div
         // with class of answer-choice, attr of the correct answer sets the HTML to the answer option A-D and then appends
-        for (var i = 0; i < answersArray.length; i++) {
+        for (var i = 0; i < questions[questionCounter].answers.length; i++) {
             answerDisplay = $("<div>");
             answerDisplay.addClass("answer-choice");
-            answerDisplay.attr(answerValue, answersArray[questionCounter][i]);
-            answerDisplay.html(answersArray[questionCounter][i]);
+            answerDisplay.attr(answerValue, indexOf(questions.answers[i]));
+            answerDisplay.html(questions[questionCounter].answers);
             $(".answer-display").append(answerDisplay);
-        }
+        };
 
         // add something to run here to grab the value of the choice the user clicks on 
-        $("answer-choice").on("click", function () {
-            userGuess = $(this).attr(answerValue);
+        $(".answer-choice").on("click", function () {
+            userGuess = $(this).val(answerValue);
             console.log(answerValue);
-        })
+        });
+    
 
-    }
+    };
 
     function theTimer() {
         // set the clock to an interval of 25 seconds
@@ -96,21 +98,22 @@ $("document").ready(function () {
     };
 
 
-    function incorrectAnswer() {
+    function wrongAnswer() {
 
     };
 
-    function generateWin() {
+    function rightAnswer() {
 
     };
 
     // this function will end up being called a lot as we need to wipe the screen clear of data
     function clear() {
-
-    }
+        $("#display-question").empty();
+        $(".answer-display").empty();
+    };
 
     // event handler for the newGame button
     $("#new-game-button").on("click", function () {
         newGame();
-    })
+    });
 });
